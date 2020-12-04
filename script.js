166988f77c3943cc001e02e3c6ef10cfd87047b3
git6497 <michaelratiarinambinina@gmail.com>
2020-12-04 11:22:48 +0100

    ajout de javascript

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function($) {
+
+    $('#btnFacebook').click(function() {
+      window.location.href = "https://dev.e-hoi.edtest.local/misael-ratiarinambinina/Testseite/cfm/apiFacebook.cfm" ;
+    })
+
+    $('#btnYoutube').click(function() {
+      window.location.href = "https://dev.e-hoi.edtest.local/misael-ratiarinambinina/Testseite/cfm/apiYoutube.cfm" ;
+    })
+
+    $('#btnInstagram').click(function() {
+      window.location.href = "https://dev.e-hoi.edtest.local/misael-ratiarinambinina/Testseite/cfm/apiInstagram.cfm" ;
+    })
+
+    $('#btnTwitter').click(function() {
+      window.location.href = "https://dev.e-hoi.edtest.local/misael-ratiarinambinina/Testseite/cfm/apiTwitter.cfm" ;
+    })
+
+
+
+});
